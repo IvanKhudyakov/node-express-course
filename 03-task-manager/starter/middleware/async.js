fa3c4e8a06dd0e6e1d26c804b2b4e9264226ac1a
Ivan Khudziakou <Ivan.Khudziakou@sap.com>
2024-08-01 01:40:33 +0300

    Week 6 code

---
+++ 03-task-manager/starter/middleware/async.js
@@ -0,0 +1,12 @@
+const asyncWrapper = (func) => {
+    return async (res, req, next) => {
+        try {
+            await func(res, req, next);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+}
+
+module.exports = asyncWrapper;
